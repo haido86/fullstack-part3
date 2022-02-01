3e52e77fc5a8850ffaef5efb89528f2e1e9a0431
haido86 <thithuhai.do@gmail.com>
2022-02-01 14:28:54 +0200

    3.15,3.16: Phonebook database, step3,4

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
 
+app.use(express.static("build"));
 app.use(express.json());
 app.use(cors());
 
@@ -13,8 +14,6 @@ morgan.token("body", (request) => JSON.stringify(request.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-
-app.use(express.static("build"));
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
@@ -29,14 +28,16 @@ app.get("/info", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
-      response.json(person);
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
     })
-    .catch((error) => {
-      response.status(400).send("Person is not found");
-    });
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -64,21 +65,35 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  Person.findByIdAndDelete(request.params.id).exec();
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.patch("/api/persons/:id", (request, response) => {
-  const _id = request.params.id;
-  Person.findByIdAndUpdate(_id, request.body, { new: true })
-    .then((res) => {
-      response.send(`${res.id} 's phone number has been updated` + res);
+app.patch("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndUpdate(request.params.id, request.body, { new: true })
+    .then((updatedPerson) => {
+      response.send(
+        `${updatedPerson.id} 's phone number has been updated ${updatedPerson}`
+      );
     })
-    .catch((error) => {
-      response.status(400).send("Person is not found");
-    });
+    .catch((error) => next(error));
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
